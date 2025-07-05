503dd12b84c96ca516a7608a82c0425cf145d2b5
MJ IT <mukisajoshuafabian@gmail.com>
2025-07-05 13:34:13 +0300

    Create no.2(b).java

---
+++ no.2(b).java
@@ -0,0 +1,6 @@
+Using String Literal:
+      String emptyString1 = ""; // Creating an empty string using a literal
+   
+Using the new Keyword:
+      String emptyString2 = new String(); // Creating an empty string using the new keyword
+   
